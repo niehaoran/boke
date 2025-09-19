e7f40641f85f13308f03c094dfdefc3220a209d7
niehaoran <179866495@qq.com>
2025-09-19 15:03:33 +0800

    更新了多个文件

--- src/config.ts
+++ src/config.ts
@@ -45,7 +45,7 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.Archive,
 		LinkPreset.About,
 		{
-			name: "不丢容器",
+			name: "不丢云",
 			url: "https://www.budiuyun.net", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},
